ec8c20127dcea8b5f63494913d1e9798af1aa7da
Wavy-34 <179828417+Wavy-34@users.noreply.github.com>
2025-01-26 20:40:29 +0100

    [wip] Im just messing with it to try what Im codding

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from sgame.bot_test import BOT
 from sgame.consts import DISCORD_TOKEN
+from sgame.game.database.insert_cards import insert_cards
+from sgame.game.database.connect import connect
 
 
 def main() -> None:
-    BOT.run(DISCORD_TOKEN)
+    conn, cursor = connect()
+    insert_cards(cursor)
+    conn.commit()
 
 
 if __name__ == "__main__":
